b43e2f83d6a3c98d5450edb0cf17cb848f63a1ea
Imade Osifo <toxsik88@gmail.com>
2020-05-13 02:19:39 -0400

    converted solution to py and ran test cases

--- bfs_dfs checker.py
+++ bfs_dfs checker.py
@@ -77,24 +77,53 @@
 #   return numberOfIslands;
 # }
 
-#   dfs(binaryMatrix, col, row-1, visited);
-#   dfs(binaryMatrix, col+1, row, visited);
-#   dfs(binaryMatrix, col, row+1, visited);
-#   dfs(binaryMatrix, col-1, row, visited);
-# }
 
-# function getNumberOfIslands(binaryMatrix) {
-#   var numberOfIslands = 0;
-#   const numberOfCols = binaryMatrix[0].length;
-#   const numberOfRows = binaryMatrix.length;
-#   const visited = new Array(numberOfRows).fill(false).map(() => new Array(numberOfCols).fill(false));
-#   for (var col = 0; col < numberOfCols; col++) {
-#     for (var row = 0; row < numberOfRows; row++) {
-#       if (binaryMatrix[col][row] === 1 && !visited[col][row]) {
-#         numberOfIslands++;
-#         dfs(binaryMatrix, col, row, visited);
-#       }
-#     }
-#   }
-#   return numberOfIslands;
-# }
+
+def dfs(binaryMatrix, col, row, visited):
+  #if it's out of bounds, don't even try
+    if (row < 0) or (col < 0) or (col >= len(binaryMatrix[0])) or (row >= len(binaryMatrix)):
+        return
+  
+  #if we've already visited, stop
+    if visited[row][col]:
+        return
+
+    if binaryMatrix[row][col] == 0:
+        return
+
+    visited[row][col] = True
+  
+    dfs(binaryMatrix, col, row-1, visited)
+    dfs(binaryMatrix, col, row+1, visited)
+    dfs(binaryMatrix, col-1, row, visited)
+    dfs(binaryMatrix, col+1, row, visited)
+    
+    
+
+
+def getNumberOfIslands(binaryMatrix): 
+    numberOfIslands = 0
+    numberOfCols = len(binaryMatrix[0])
+    numberOfRows = len(binaryMatrix)
+    visited = [[False for _ in range(numberOfCols)] for _ in range(numberOfRows)]
+    
+    row = 0
+    while row < numberOfRows: 
+        col = 0
+        while col < numberOfCols:
+            if ((binaryMatrix[row][col] == 1) and not (visited[row][col])):
+                numberOfIslands += 1
+                dfs(binaryMatrix, col, row, visited)
+            col += 1 
+        row += 1
+    
+    return numberOfIslands
+
+arr =  [[0,1,1,0,1],
+        [0,0,1,1,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,1]]
+
+print(getNumberOfIslands(arr))
+# print(arr[0][3])
