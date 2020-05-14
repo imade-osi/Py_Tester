41c0ed173b007dad01639dfa69d7f83d53fa6d8c
Imade Osifo <toxsik88@gmail.com>
2020-05-14 18:28:49 -0400

    changed logic and added comments

--- py_tester.py
+++ py_tester.py
@@ -439,27 +439,27 @@
         
 #   return solutions_arr
 
-Island Count
-This problem is very similar to counting the number of connected components in an Undirected Graph. However, Graph Theory isn’t necessary to solve this problem or to understand its solution. In an undirected graph, a connected component is a group of vertices in which every vertex is connected to at least one other vertex. In a similar way, an island in the matrix is a group of adjacent (connected) 1s.
+# Island Count
+# This problem is very similar to counting the number of connected components in an Undirected Graph. However, Graph Theory isn’t necessary to solve this problem or to understand its solution. In an undirected graph, a connected component is a group of vertices in which every vertex is connected to at least one other vertex. In a similar way, an island in the matrix is a group of adjacent (connected) 1s.
 
-To solve this problem, we’ll traverse binaryMatrix and every time we come across a cell of 1 we’ll do the following: Change that cell and all its vertically and horizontally (but not diagonally) adjacent 1s into -1s. We do this “expansion” in order to avoid recounting of islands. Increment islands - which is our counter for number of islands - by 1. Expanding from a cell whose value is 1 to other adjacent 1s in binaryMatrix is similar to running a Breadth-First Search (BFS) or a Depth-First Search (DFS).
+# To solve this problem, we’ll traverse binaryMatrix and every time we come across a cell of 1 we’ll do the following: Change that cell and all its vertically and horizontally (but not diagonally) adjacent 1s into -1s. We do this “expansion” in order to avoid recounting of islands. Increment islands - which is our counter for number of islands - by 1. Expanding from a cell whose value is 1 to other adjacent 1s in binaryMatrix is similar to running a Breadth-First Search (BFS) or a Depth-First Search (DFS).
 
-In our case, we’ll avoid using a recursion and instead opt for an iterative approach to expand to all adjacent 1s. We do so by using queue that holds the next indices to visit. We keep expanding to other adjacent 1s as long as the queue is not empty. Whenever we encounter a value of -1 in our traversal, we ignore it since it is part on an island we’ve already counted.
+# In our case, we’ll avoid using a recursion and instead opt for an iterative approach to expand to all adjacent 1s. We do so by using queue that holds the next indices to visit. We keep expanding to other adjacent 1s as long as the queue is not empty. Whenever we encounter a value of -1 in our traversal, we ignore it since it is part on an island we’ve already counted.
 
-Pseudocode:
+# Pseudocode:
 
-function getNumberOfIslands(binaryMatrix):
-    islands = 0
-    rows = binaryMatrix.length # number of rows
-    cols = binaryMatrix[0].length # number of columns
+# function getNumberOfIslands(binaryMatrix):
+#     islands = 0
+#     rows = binaryMatrix.length # number of rows
+#     cols = binaryMatrix[0].length # number of columns
 
-    for i from 0 to rows-1:
-        for j from 0 to cols-1:
-            if (binaryMatrix[i][j] == 1):
-                markIsland(binaryMatrix, rows, cols, i, j)
-                islands++
+#     for i from 0 to rows-1:
+#         for j from 0 to cols-1:
+#             if (binaryMatrix[i][j] == 1):
+#                 markIsland(binaryMatrix, rows, cols, i, j)
+#                 islands++
                 
-    return islands
+#     return islands
 
 
 function markIsland(binaryMatrix, rows, cols, i, j):
