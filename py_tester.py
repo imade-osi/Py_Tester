5b4041d2dff561c625d45438c821be720c2948e5
Imade Osifo <toxsik88@gmail.com>
2020-05-16 17:11:06 -0400

    recreated logic for root num

--- py_tester.py
+++ py_tester.py
@@ -571,15 +571,15 @@
 
 # Iteration 2: Since we concluded that 0 < y < 3.5, our second member of the sequence should be, again, the average between our bounds 0 and 3.5 - approxRoot= 1.75. This time 1.753 ≅ 5.3 < 7, which means 1.75 < y < 3.5, so we update lowerBound = 1.75. The error is bounded by: |y - approxRoot| < |upperBound - lowerBound|= |3.5- 1.75| = 1.75. We continue the sequence this way, and notice that the error bound is reduced by half by every iteration. Here is a summary of the first few iterations in this method:
 
-# # Iteration	LowerBound	UpperBound	approxRoot value	|y- approxRoot| - True Error	Error bound
-# 1	0	7	3.5	≅ 1.58	< 3.5 (7×2-1)
-# 2	0	3.5	1.75	≅0.16	< 1.75 (7×2-2)
-# 3	1.75	3.5	2.625	≅0.71	< 0.875 (7×2-3)
-# 4	1.75	2.625	2.1875	≅0.27	< 0.4375 (7×2-4)
-# As we can see, by choosing the average value between By the 13th iteration, we’ll reach an error bound of 7×2^(-13), which is less than 0.001 (notice that the error may be much smaller, but since we don’t actually know the real value of y in the process of computation, we cannot promise a better bound).
+# Iteration	LowerBound	UpperBound	approxRoot value	|y- approxRoot| - True Error	Error bound
+1	0	7	3.5	≅ 1.58	< 3.5 (7×2-1)
+2	0	3.5	1.75	≅0.16	< 1.75 (7×2-2)
+3	1.75	3.5	2.625	≅0.71	< 0.875 (7×2-3)
+4	1.75	2.625	2.1875	≅0.27	< 0.4375 (7×2-4)
+As we can see, by choosing the average value between By the 13th iteration, we’ll reach an error bound of 7×2^(-13), which is less than 0.001 (notice that the error may be much smaller, but since we don’t actually know the real value of y in the process of computation, we cannot promise a better bound).
 
-# Time Complexity: notice that every loop iteration is done in O(1), under the assumption that the power function takes a constant time. The initial error is x, and the error is multiplied by 0.5 in every iteration. Thus the number of iterations is the minimal k such that: 2^(-k) x<0.001 i.e. 2^(-k)<(0.001 / x) k >log(x) + 3log(10) = O(log(x)) The number of iterations is therefore O(log(x)), meaning the total runtime is O(log(x)) if we refer to the value stored in x, or O(x) if we refer to the number of bits required to represent x (since it takes Log(x) in average bits to represent a number x).
+Time Complexity: notice that every loop iteration is done in O(1), under the assumption that the power function takes a constant time. The initial error is x, and the error is multiplied by 0.5 in every iteration. Thus the number of iterations is the minimal k such that: 2^(-k) x<0.001 i.e. 2^(-k)<(0.001 / x) k >log(x) + 3log(10) = O(log(x)) The number of iterations is therefore O(log(x)), meaning the total runtime is O(log(x)) if we refer to the value stored in x, or O(x) if we refer to the number of bits required to represent x (since it takes Log(x) in average bits to represent a number x).
 
-# Space Complexity: O(1), since we only need a constant number of variables for the algorithm.
+Space Complexity: O(1), since we only need a constant number of variables for the algorithm.
 
-# Mathematical Note: our explanation used the fact that power function x^n is a monotonically increasing continuous function on the positive numbers. Although it is true, the Intermediate value theorem in calculus, states that this method will work for every continuous function - but the proof for this requires more mathematical tools.
+Mathematical Note: our explanation used the fact that power function x^n is a monotonically increasing continuous function on the positive numbers. Although it is true, the Intermediate value theorem in calculus, states that this method will work for every continuous function - but the proof for this requires more mathematical tools.
