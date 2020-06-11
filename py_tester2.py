f228d544627597abf58cbab67fc6a09dfa376b73
Imade Osifo <toxsik88@gmail.com>
2020-06-10 23:10:52 -0400

    edited out tester and debuggers for algos

--- py_tester2.py
+++ py_tester2.py
@@ -223,8 +223,8 @@
 #     # 160
 #   return res
 
-print(array_of_array_products([1]) 
-print(array_of_array_products([3]) 
+# print(array_of_array_products([1]) 
+# print(array_of_array_products([3]) 
 print(array_of_array_products([2]) 
 
     
