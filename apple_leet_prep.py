624c3e3ee908937e43c4d860b3ae243a04937fca
Imade Osifo <toxsik88@gmail.com>
2020-07-13 22:30:19 -0400

    updated algo logic for unique triplets

--- apple_leet_prep.py
+++ apple_leet_prep.py
@@ -22,3 +22,28 @@ class Solution:
             curr = temp_next
 
         return prev
+
+
+#given [2,3,-1,2,4,8], target = 0
+#         ^
+#output  [[1,2,3], [2,3,4], [3,2,1]]
+
+
+#novice solution
+
+#start at first index of arr
+#iterate through array for values after first index
+# perform two sum operations on sub array
+# collect triplet into return array if found
+# move onto next index in outer loop
+
+
+def find_unique_triplets(arr, target):
+
+    trip_hash = {}
+
+    for outer_ele in arr:
+
+        for inner in
+
+    return []
