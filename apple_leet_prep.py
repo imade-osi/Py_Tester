928674e6b365904f8b257321268f17ce7e7a0e8b
Imade Osifo <toxsik88@gmail.com>
2020-07-17 22:56:40 -0400

    edited fibonacci logic in return statements

--- apple_leet_prep.py
+++ apple_leet_prep.py
@@ -58,13 +58,13 @@ def recurse_prac(n):
     #recurse_prac(n-1)
 
 
-def fib(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
+# def fib(n):
+#     if n == 0:
+#         return 1
+#     elif n == 1:
+#         return 1
     
-    return fib(n-1) + fib(n-2)
+#     return fib(n-1) + fib(n-2)
 
 
 def linear_fib(n):
