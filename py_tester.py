4d7a5cfb3516f61c5854c3a97a876ea47990338c
Imade Osifo <toxsik88@gmail.com>
2020-08-09 22:04:03 -0400

    restructured print statements for algos checks

--- py_tester.py
+++ py_tester.py
@@ -288,7 +288,7 @@
 # def single_checker(my_list):
 #     my_hash = {}
 
-#     for ele in my_list
+#     for ele in my_list:
 #         if ele in my_hash:
 #             my_hash.pop(ele)
 #         else:
@@ -296,7 +296,7 @@
 #     return my_hash.keys()
         
 
-# print(single_checker([3,4,5,5,3,4,1,2,2]))
+# print(single_checker([3,4,5,5,3,4,1,2]))
 
 # def single_checker(my_list):
 #     my_list.sort()
