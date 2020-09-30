1127fc90de5055ea895958e9c6d4ace5e085d238
Imade Osifo <toxsik88@gmail.com>
2020-09-29 20:39:00 -0400

    added memoization  logic for teaching

--- py_tester2.py
+++ py_tester2.py
@@ -364,24 +364,24 @@
 #          //  \\ 
 #       AAAB    0
 
-"1112"  
-   ^      
+# "1112"  
+#    ^      
 
 
-cache = {}
-def decode_ways(code, inx):
-  if inx == len(code):
-    return 1
+# cache = {}
+# def decode_ways(code, inx):
+#   if inx == len(code):
+#     return 1
 
-  if inx not in cache:
-    way1 = decode_ways(code, inx+1)
-    if int(code[inx:inx+2]) <= 26 and inx + 2 <= len(code):
-      way2 = decode_ways(code, inx+2)
-    else:
-      way2 = 0
-    cache[inx] = way1 + way2
+#   if inx not in cache:
+#     way1 = decode_ways(code, inx+1)
+#     if int(code[inx:inx+2]) <= 26 and inx + 2 <= len(code):
+#       way2 = decode_ways(code, inx+2)
+#     else:
+#       way2 = 0
+#     cache[inx] = way1 + way2
 
-  return cache[inx]
+#   return cache[inx]
 
   
   
