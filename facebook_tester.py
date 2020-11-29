ab800d5cd2f41fa7a543592a896a80a94af3128e
Imade Osifo <toxsik88@gmail.com>
2020-11-28 20:32:47 -0500

    checked for py version

--- facebook_tester.py
+++ facebook_tester.py
@@ -1,3 +1,9 @@
-a = [float(x) for x in range(10)]
+# import numpy as ny
+import sys
+# a = [float(x) for x in range(10, 21) if x %2 == 0]
+# print(f'array a:{a}')
 
-print(f'array a:{a}')
+# n = np.empty(0)
+# print(n)
+
+print(sys.version)
