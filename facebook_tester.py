cb55555b71e7343bbeb837bde9873f8c85aeab1d
Imade Osifo <toxsik88@gmail.com>
2020-11-29 23:44:02 -0500

    addded rotated matrix code

--- facebook_tester.py
+++ facebook_tester.py
@@ -1,5 +1,18 @@
 # a = [float(x) for x in range(10, 21) if x %2 == 0]
 # print(f'array a:{a}')
 
-# n = np.empty(0)
-# print(n)
+
+# b = [[x for x in range(4)] for j in range(4) ]
+# print(f'array b: {b}')
+
+# a = [[1,2,3], 
+#  [4,5,6], 
+#  [7,8,9]]
+
+# def rotateImage(a):
+#     a.reverse()
+#     for i in range(len(a)):
+#         for j in range(i):
+#             a[i][j], a[j][i] = a[j][i], a[i][j]
+#     return a
+
