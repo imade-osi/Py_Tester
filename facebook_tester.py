d379780644b6f908056cce979db968a35eb0441b
Imade Osifo <toxsik88@gmail.com>
2020-11-30 22:26:07 -0500

    updated reverse function

--- facebook_tester.py
+++ facebook_tester.py
@@ -5,9 +5,12 @@
 # b = [[x for x in range(4)] for j in range(4) ]
 # print(f'array b: {b}')
 
-# a = [[1,2,3], 
-#  [4,5,6], 
-#  [7,8,9]]
+a = [[1,2,3], 
+    [4,5,6], 
+    [7,8,9]]
+
+rev = a.reverse()
+print(rev)
 
 # def rotateImage(a):
 #     a.reverse()
