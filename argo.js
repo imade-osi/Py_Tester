caf4a80c87775c51d610bd317b40a001f241ead4
Imade Osifo <toxsik88@gmail.com>
2020-12-21 20:46:48 -0500

    edited sort by name

--- argo.js
+++ argo.js
@@ -44,7 +44,20 @@ items.sort(function (a, b) {
 });
 
 
+// sort by name
+items.sort(function (a, b) {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
 
+    // names must be equal
+    return 0;
+});
 
 
 
